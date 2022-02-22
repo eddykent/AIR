c8b541a63b69d6648bcfc99edc5fd74226e59571
Ed Kent <edward-r-kent@hotmail.co.uk>
2022-02-22 22:03:50 +0000

    first attempt at the bat harmonic pattern

--- tests/harmonic_pattern_test.py
+++ tests/harmonic_pattern_test.py
@@ -61,7 +61,7 @@ database_response = cur.fetchall()
 #OurChartPattern = SupportAndResistance
 #OurChartPattern = ParallelChannelBreakout
 
-OurChartPattern = Cypher
+OurChartPattern = Bat
 
 chart_pattern = OurChartPattern()
 
