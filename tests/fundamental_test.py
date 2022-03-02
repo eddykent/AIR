579e43741e4cd1ef480b23c51b779e98ff30ba45
Ed Kent <edward-r-kent@hotmail.co.uk>
2022-03-02 09:26:37 +0000

    forgot to add fundamental_test.py to the src tree

---
+++ tests/fundamental_test.py
@@ -0,0 +1,31 @@
+import datetime
+import pdb
+
+
+from utils import ListFileReader
+from fundamental import *
+import scrape.feed_collector as feedco
+
+
+
+assert __name__ != "__main__", "You must run tests through the run_test.py hoister" #need to remind myself! :) 
+
+	
+lfr = ListFileReader()
+fx_pairs = lfr.read('fx_pairs/fx_mains.txt')
+cs = ClientSentiment(fx_pairs)##put fx pairs in!
+cs.fetch()
+	
+
+#rss = feedco.RSSCollect(lfr.read('sources/rss_feeds.txt'))
+#keyword_mappings_example = [
+#	('USD',['FEDERAL RESERVE', 'US DOLLAR', 'GREEN BACK', 'GREENBACK', 'USD']),
+#	('GBP/USD',['GBP/USD','GBPUSD','GBP','usd','CABLE'])  #put after USD to be detected second - but usd matches USD so all the other keys can be added (but in lower case) to this
+	#which is why helper is needed! :) 
+#]
+#rss.parse_feeds()
+#kwh = KeywordMapHelper()
+#aa = ArticleAnalysis(kwh)
+#rss.analyse_articles(aa)
+#rss.collect(kwh)
+
