b78c46e6d0dfea60130ab67a2b0f238a8c99c82e
Ed Kent <edward-r-kent@hotmail.co.uk>
2022-03-08 06:52:17 +0000

    removed the txt file from indicators and added new select_indicators.py which will be full of techniques to select the indicators we want

---
+++ indicators/select_indicators.py
@@ -0,0 +1,5 @@
+#there are too many indicators! 
+#use a PCA technique or investigate correlations to find best indicators to use, based on their similarity to eachother
+#we want to aim for capturing all the variance across all indicators without removing too much info 
+
+
