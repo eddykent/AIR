46926ee5f4972f6621c976ba600d5652bc777910
Ed Kent <edward-r-kent@hotmail.co.uk>
2022-03-17 15:06:42 +0000

    added all news archive tools to getting historic news regarding forex

---
+++ web/news_archives/dailyfx.py
@@ -0,0 +1,142 @@
+
+
+
+
+
+import pdb
+
+from archive_head import *
+
+#from all_project import * --import everything from the project 
+from web.scraper import DailyFXNews #scraper for dailyfx news stories
+
+
+##a DailyFX archive reader to place their old news stories into the database
+#if no fx pair is present in the title then it will not be stored
+monthly_url_str = 'https://www.dailyfx.com/archive/{year:0>4}/{month:0>2}'
+
+#first, get all hyperlinks that can lead to a story
+hyperlinks = []
+
+the_date = datetime.datetime.now()
+this_year = the_date.year
+this_month = the_date.month
+earliest_year = 2018
+years = [y for y in range(earliest_year,this_year)]
+months = [m for m in range(1,13)]
+
+months_years = [(m,y) for m in months for y in years] + [(m,this_year) for m in months if m < this_month]
+monthly_urls = [monthly_url_str.format(year=y,month=m) for (m,y) in months_years]
+
+source_title = 'DailyFX - Market News'
+source_ref = 'dailyfx.com'
+
+
+class DailyFXArchive(Scraper):
+	
+	def scrape(self):
+		
+		#get all sections so we can create a list of hyperlinked 
+		sections = self.html.xpath("//div[@class='dfx-archiveList']/section[@class='my-6']")
+		#pdb.set_trace()
+		article_data = []
+		for section in sections:	
+			date_header = section.xpath(".//h2[contains(@class,'text-black')]")
+			article_list = section.xpath(".//div[contains(@class,'dfx-articleList')]")
+			if date_header and article_list:
+				date_str = date_header[0].text #turns out this isnt needed
+				article_hyperlinks = article_list[0].xpath(".//a[contains(@class,'dfx-articleListItem')]")
+				for article_hyperlink in article_hyperlinks:
+					#pdb.set_trace()
+					
+					article_url = article_hyperlink.attrs.get('href') if article_hyperlink.attrs else None
+					
+					title = article_hyperlink.xpath(".//span[contains(@class,'dfx-articleListItem__title')]")
+					the_time= article_hyperlink.xpath(".//span[contains(@class,'jsdfx-articleListItem__date')]")
+					author = article_hyperlink.xpath(".//span[contains(@class,'jsdfx-articleListItem__author')]")
+					
+					article_title = title[0].text
+					article_time = the_time[0].text
+					article_author = author[0].text.replace(' by ',' ')[1:].strip()
+					
+					article_data.append({
+						'title':article_title,
+						'the_date':th.from_str_2(article_time),
+						'author':article_author,
+						'link':article_url,
+						'source_ref':source_ref,
+						'source_title':source_title
+					})
+					
+		#pdb.set_trace()			
+		return article_data	
+
+
+article_data = [] 
+relevant_articles = []
+print('Fetching news story URLs from the archives...')
+for archive_url in tqdm(monthly_urls):
+	dfxa = DailyFXArchive(archive_url) #loadiing bar?
+	article_data.extend(dfxa.scrape())
+
+print('Determining relevant stories...')
+for article_d in tqdm(article_data):	
+	relevant = keyword_map.relevant_keys(article_d['title'])
+	if not relevant:
+		continue 
+	relevant_articles.append(article_d)
+
+#pdb.set_trace()
+print('Reading full stories... ')
+#relevant_articles = relevant_articles[:100]
+read_stories = []
+for article_d in tqdm(relevant_articles):
+	dfx = DailyFXNews(article_d['link'])
+	full_text = dfx.scrape()
+	if full_text and full_text != 'VIDEO':
+		article_d['full_text'] = full_text
+		read_stories.append(article_d)
+
+print('Saving stories to database...')
+chunk = 1000 #save in chunks of 1000 or something
+partition_articles = [read_stories[i:i+chunk] for i in range(0,len(read_stories),chunk)]  
+for partition in tqdm(partition_articles):
+	articles = [Article.from_dict(article_d) for article_d in partition]
+	database_rows = [a.to_database_row() for a in articles]
+	sql_rows = [cur.mogrify(Article.sql_row,db_row).decode() for db_row in database_rows]
+	hash_checks = [db_row['hash_check'] for db_row in database_rows]
+	article_dump = ','.join(sql_rows)
+	cur.execute(upsert_query,{'articles':Inject(article_dump), 'remove_hashes':hash_checks})
+
+cur.con.commit()
+cur.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
---
+++ web/news_archives/fxstreet.py
@@ -0,0 +1,118 @@
+
+import pdb
+
+from archive_head import *
+
+#from all_project import * --import everything from the project 
+from web.scraper import FXStreet #scraper for fxstreet news stories
+
+daily_url_str = 'https://www.fxstreet.com/news/all?date={year:0>4}-{month:0>2}-{day:0>2}'
+
+source_ref = 'fxstreet.com'
+source_title = 'fxstreet.com'
+
+days_back = 1530
+today = datetime.datetime.now()
+today = datetime.datetime(today.year,today.month,today.day)
+start_day = today - datetime.timedelta(days=50)
+
+all_days = [start_day-datetime.timedelta(days=i) for i in range(0,days_back)]
+
+class FXStreetArchive(Scraper):
+	
+	def scrape(self):
+		article_data = [] #here wesef can only get the title and the link 
+		article_heads = self.html.xpath("//div[contains(@class,'fxs_crawling_page')]//article")
+		for article_head in article_heads:
+			anchor = article_head.xpath('.//a')[0]
+			link = anchor.attrs.get('href')
+			title = anchor.attrs.get('title')
+			time = article_head.xpath('.//time')[0].text
+			article_data.append({
+				'time':time,
+				'link':link,
+				'title':title,
+				'source_title':source_title,
+				'source_ref':source_ref
+			})
+		return article_data
+
+class FXStreetExtra(Scraper): #make a class that reads a bit more stuff from the news article since we cant get it from the archive
+	
+	def scrape(self):	
+		#author, summary, full_text
+		summary_bits = self.html.xpath("//div[@class='fxs_article_content']/ul/li/strong")
+		summary = '\n'.join([sb.text for sb in summary_bits])
+		
+		full_text_bits = self.html.xpath("//div[@class='fxs_article_content']/p")
+		full_text = '\n'.join([fb.text for fb in full_text_bits])
+		
+		author_elem = self.html.xpath("//article[@class='fxs_article']/header/span/a[@data-gtmid='lateralnavigation-post-author']")
+		author = author_elem[0].text
+		
+		return full_text, author, summary 
+
+article_data = []
+print('Fetching news story URLs from the archives...')
+for daily in tqdm(all_days):
+	daily_url = daily_url_str.format(year=daily.year,month=daily.month,day=daily.day)
+	fxsa = FXStreetArchive(daily_url)
+	this_day_articles = fxsa.scrape()
+	for this_day_article in this_day_articles:
+		this_day_article['the_date'] = daily
+	article_data.extend(this_day_articles)
+
+relevant_articles = []
+print('Determining relevant stories...')
+for article_d in tqdm(article_data):	
+	relevant = keyword_map.relevant_keys(article_d['title'])
+	if not relevant:
+		continue 
+	relevant_articles.append(article_d)
+
+print('Fixing dates...')
+for article_d in tqdm(relevant_articles):
+	h, m = article_d['time'].split(',')[1][:-3].strip().split(':') #
+	base_date = article_d['the_date']
+	article_d['the_date'] = datetime.datetime(base_date.year,base_date.month,base_date.day,int(h),int(m))
+	del article_d['time']
+
+print('Reading full stories... ')
+read_stories = []
+for article_d in tqdm(relevant_articles):
+	dfx = FXStreetExtra(article_d['link'])
+	full_text, author, summary = dfx.scrape()
+	if full_text and full_text != 'VIDEO':
+		article_d['full_text'] = full_text
+		article_d['author'] = author
+		article_d['summary'] = summary
+		read_stories.append(article_d)
+	
+print('Saving stories to database...')
+chunk = 1000 #save in chunks of 1000 or something
+partition_articles = [read_stories[i:i+chunk] for i in range(0,len(read_stories),chunk)]  
+for partition in tqdm(partition_articles):
+	articles = [Article.from_dict(article_d) for article_d in partition]
+	database_rows = [a.to_database_row() for a in articles]
+	sql_rows = [cur.mogrify(Article.sql_row,db_row).decode() for db_row in database_rows]
+	hash_checks = [db_row['hash_check'] for db_row in database_rows]
+	article_dump = ','.join(sql_rows)
+	cur.execute(upsert_query,{'articles':Inject(article_dump), 'remove_hashes':hash_checks})
+
+cur.con.commit()
+cur.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
