b52e15066c70ccfdc0603f347919927acde66b2d
Ed Kent <edward-r-kent@hotmail.co.uk>
2022-03-30 22:48:45 +0100

    forgot to add deleted file to prev commit

--- neural_net.py
+++
@@ -1,4 +0,0 @@
-
-##module for containing neural networks. handle training and trade setup info from this module
-##perhaps model and weight files etc can go in a separate folder. Just provide some key info 
-##like what date, what we are doing (training or inference) etc
