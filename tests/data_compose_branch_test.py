4497288e174392d21b27cb5f179ef7e54db49253
Ed Kent <edward-r-kent@hotmail.co.uk>
2022-04-12 17:51:46 +0100

    took out wrong resulting queries in the data compose tests

--- tests/data_compose_branch_test.py
+++ tests/data_compose_branch_test.py
@@ -30,18 +30,3 @@ with Database(commit=False, cache=False) as cursor:
 	result = composer.result(as_json=True)
 
 
-
-'''
---TEST
-DROP TABLE IF EXISTS candles_tmp CASCADE;
-DROP TABLE IF EXISTS close_prices_tmp CASCADE; 
-DROP TABLE IF EXISTS rsi_tmp CASCADE; 
-DROP TABLE IF EXISTS currency_strengths_tmp CASCADE;
-DROP TABLE IF EXISTS simple_moving_averages_tmp CASCADE;
-SELECT * INTO candles_tmp FROM get_candles_from_currencies(ARRAY['EUR','USD','JPY','AUD','NZD','CAD','CHF','GBP'], '07 Mar 2022 12:30:00'::timestamp, 100, 15);
-SELECT row_index, full_name, the_date, close_price AS value INTO close_prices_tmp FROM candles_close_price('candles_tmp');
-SELECT row_index, full_name, the_date, rsi AS value INTO rsi_tmp FROM values_relative_strength_index('close_prices_tmp', 14, 1);
-SELECT row_index, currency AS full_name, the_date, currency_strength AS value INTO currency_strengths_tmp FROM values_currency_strength('rsi_tmp');
-SELECT row_index, full_name, the_date, value INTO simple_moving_averages_tmp FROM values_simple_moving_average('currency_strengths_tmp',3);
-SELECT * FROM values_instrument_ranking('simple_moving_averages_tmp');
-'''
