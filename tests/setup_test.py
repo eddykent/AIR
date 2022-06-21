6a34a8b6918d96373af70e061eea5c83286c8307
Ed Kent <edward-r-kent@hotmail.co.uk>
2022-06-21 16:20:38 +0100

    forgot to add test for the setup

--- tests/setup_test.py
+++ tests/setup_test.py
@@ -1,20 +1,24 @@
 
 import datetime
 import pdb
+from collections import Counter
 # test the setup object and also test its trade signals 
 
-from setups.setups1 import BB_KC_RSI
+from setups.setups1 import BB_KC_RSI, ADX_EMA_RSI
 from utils import ListFileReader, Database
 from backtest import BackTesterDatabase
 
 lfr = ListFileReader()
 
 
-start_date = datetime.datetime(2022,1,11)
-end_date = datetime.datetime(2022,1,20)
+start_date = datetime.datetime(2022,6,13)
+end_date = datetime.datetime(2022,6,17)
 instruments = lfr.read('fx_pairs/fx_mains.txt')
 
-bbckrsi = BB_KC_RSI(instruments)
+#bbckrsi = BB_KC_RSI(instruments) 
+bbckrsi = ADX_EMA_RSI(instruments)
+
+
 signals = bbckrsi.get_setups(start_date,end_date)
 
 
@@ -23,5 +27,8 @@ signals = bbckrsi.get_setups(start_date,end_date)
 cursor = Database(cache=False,commit=False)
 btd = BackTesterDatabase(cursor)
 result = btd.perform(signals)
+statuses = [r.result_status for r in result]
+cc = Counter(statuses)
 pdb.set_trace()
-print(len(result))
+print(cc)
+print(result)
