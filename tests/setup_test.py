a0e6e8f2688cd81bb5545bf3b7af484cb0244dff
Ed Kent <edward-r-kent@hotmail.co.uk>
2022-12-07 20:50:34 +0000

    added setup test

--- tests/setup_test.py
+++ tests/setup_test.py
@@ -5,11 +5,12 @@ from collections import Counter
 import pickle
 
 # test the setup object and also test its trade signals 
-from setups.setup_tools import CandleDataTool, PipStop
+from setups.setup_tools import CandleDataTool, PipStop, ATRStop
 from setups.setups1 import BB_KC_RSI, ADX_EMA_RSI, HA_VWAP_RSI_DIVERGENCE
 #from setups.custom_setups import Harmony
 from setups.collected_setups import Harmony 
 from setups.simple_setups import *
+from setups.trade_pro import *
 from utils import ListFileReader, Database
 from backtest import BackTesterDatabase, BackTestStatistics
 from filters.simple_filters import LambdaSelectFilter
@@ -28,29 +29,33 @@ currencies = lfr.read('fx_pairs/currencies.txt')
 #axemrsi = ADX_EMA_RSI()
 
 
-#datatool = CandleDataTool() 
-#datatool.start_date = datetime.datetime(2022,5,4)
-#datatool.end_date = datetime.datetime(2022,9,4)
-#datatool.instruments = lfr.read('fx_pairs/fx_mains.txt')
-#datatool.volumes = True
-#dbf.stopwatch('fetch candles')
-#datatool.read_data_from_currencies(currencies)
-#tsd = datatool.get_trade_signalling_data()
-#dbf.stopwatch('fetch candles')
+datatool = CandleDataTool() 
+datatool.start_date = datetime.datetime(2022,5,4)
+datatool.end_date = datetime.datetime(2022,9,4)
+datatool.instruments = lfr.read('fx_pairs/fx_mains.txt')
+datatool.volumes = True
+datatool.chart_resolution = 60
+#datatool.candle_offset = 15
+dbf.stopwatch('fetch candles')
+datatool.read_data_from_currencies(currencies)
+tsd = datatool.get_trade_signalling_data()
+dbf.stopwatch('fetch candles')
 #
 #with open('./data/pickles/setup_test_candles.plk','wb') as fh:
 #	pickle.dump(tsd,fh)
 #
-with open('./data/pickles/setup_test_candles.pkl','rb') as fh:
-	tsd = pickle.load(fh)
+#with open('./data/pickles/setup_test_candles.pkl','rb') as fh:
+#	tsd = pickle.load(fh) #15m candles
 
 
-fxss = MediumScalpDaviddAnthony() 
+fxss = SIMPLE_MONEY() 
 #signals = mrffx.get_setups(start_date,end_date) #+ axemrsi.get_setups(start_date,end_date)
 #msda = HA_VWAP_RSI_DIVERGENCE()
 #msda.stop_calculator = PipStop(take_profit_pips=30,stop_loss_pips=20)
+#fxss.stop_calculator = ATRStop()
 fxss.stop_calculator = PipStop(take_profit_pips=30,stop_loss_pips=20)
 
+#pdb.set_trace()
 signals = fxss.get_setups(tsd)
 
 #pdb.set_trace()
