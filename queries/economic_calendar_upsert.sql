8d973a04c862880be2c0f07f26dd7e563189073d
Ed Kent <edward-r-kent@hotmail.co.uk>
2023-03-05 16:22:15 +0000

    changed eco calendar upsert to now update and persist guids

--- queries/economic_calendar_upsert.sql
+++ queries/economic_calendar_upsert.sql
@@ -3,13 +3,37 @@
 WITH new_rows AS (
 	SELECT * FROM (VALUES 
 		%(calendar_events)s
-	) AS rows(the_date,impact,country,description,actual,previous,consensus,forecast)
+	) AS rows(the_date,source_ref,impact,country,currency,description,actual,previous,consensus,forecast)
 ),
-delete_old AS (
-	DELETE FROM economic_calendar ec USING new_rows nr
-	WHERE ec.the_date = nr.the_date AND ec.country = nr.country AND ec.description = nr.description
+update_old AS ( --update to persist the guid in the database
+	UPDATE economic_calendar AS ec
+	SET 
+	impact = nr.impact,
+	currency = nr.currency,
+	actual = nr.actual,
+	previous = nr.previous,
+	consensus = nr.consensus,	
+	forecast = nr.forecast
+	FROM new_rows nr
+	WHERE 
+		ec.source_ref = nr.source_ref AND 
+		ec.the_date = nr.the_date AND 
+		ec.country = nr.country AND 
+		ec.description = nr.description
+	RETURNING ec.source_ref, ec.the_date, ec.country, ec.description
 )
-INSERT INTO economic_calendar(the_date,impact,country,description,actual,previous,consensus,forecast)
-SELECT the_date,impact,country,description,actual,previous,consensus,forecast 
-FROM new_rows 
-RETURNING the_date,country,description
+INSERT INTO economic_calendar(the_date,source_ref,impact,country,currency,description,actual,previous,consensus,forecast)
+SELECT the_date,source_ref,impact,country,currency,description,actual,previous,consensus,forecast 
+FROM new_rows nr
+WHERE NOT EXISTS (
+	SELECT 1 FROM update_old uo
+	WHERE nr.source_ref = uo.source_ref AND
+	nr.the_date = uo.the_date AND
+	nr.country = uo.country AND
+	nr.description = uo.description
+)
+
+
+
+
+
