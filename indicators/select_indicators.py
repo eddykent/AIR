46829ffc86962e2c7b66b1bb44d8afb1077e3044
Edward Kent <edward-r-kent@hotmail.co.uk>
2026-02-26 13:53:27 +0000

    continnue move

--- indicators/select_indicators.py
+++
@@ -1,5 +0,0 @@
-#there are too many indicators! 
-#use a PCA technique or investigate correlations to find best indicators to use, based on their similarity to eachother
-#we want to aim for capturing all the variance across all indicators without removing too much info 
-#perhaps move this to another folder called composition or something that we use for creating strategies (extra from setups) 
-
